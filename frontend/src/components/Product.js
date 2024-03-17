fbff342e81b73bbee032affc34907c13adab3ad1
DucTrungDo <ducdt2192@gmail.com>
2024-03-17 17:38:32 +0700

    Test push

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -9,7 +9,6 @@ const Product = ({ product }) => {
       <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
       </Link>
-
       <Card.Body>
         <Link to={`/product/${product._id}`}>
           <Card.Title as='div'>
@@ -23,7 +22,6 @@ const Product = ({ product }) => {
             text={`${product.numReviews} reviews`}
           />
         </Card.Text>
-
         <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
     </Card>
