7da5c5882f3ae182ceb7a9f9d4049168eb05f299
DucTrungDo <ducdt2192@gmail.com>
2024-03-17 17:25:14 +0700

    Add Home, Product Detail Screen UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
 import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <Fragment>
-      <h1>Welcome to iTechShop</h1>
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Header />
+        <main className='py-3'>
+          <Container>
+            <Routes>
+              <Route path='/' element={<HomeScreen />} />
+              <Route path='/product/:id' element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Fragment>
+    </Router>
   )
 }
 
